843e16dd747b78eb68ae7c3e15f80f95da254ebc
luiz-otavio <luizfarias2006@hotmail.com>
2022-01-03 23:07:50 -0300

    fix: forgot to register `PartyCommand` and `PartyHandler`

--- api/src/main/kotlin/com/nationcraft/party/PartyFrame.kt
+++ api/src/main/kotlin/com/nationcraft/party/PartyFrame.kt
@@ -1,21 +1,32 @@
 package com.nationcraft.party
 
+import com.nationcraft.party.command.PartyCommand
+import com.nationcraft.party.listener.PartyHandler
+import com.nationcraft.party.repository.party.PartyRepository
+import me.saiintbrisson.bukkit.command.BukkitFrame
 import me.saiintbrisson.minecraft.ViewFrame
+import org.bukkit.Bukkit
 import org.bukkit.plugin.Plugin
 
 class PartyFrame(
-    private val plugin: Plugin
+    plugin: Plugin
 ) {
 
     private val viewFrame: ViewFrame
+    private val bukkitFrame: BukkitFrame
 
     init {
         viewFrame = ViewFrame(plugin)
 
-        viewFrame.register(
+        bukkitFrame = BukkitFrame(plugin)
 
+        bukkitFrame.registerCommands(
+            PartyCommand()
         )
 
+        Bukkit.getPluginManager().registerEvents(PartyHandler(), plugin)
     }
+
+    fun getRepository() = PartyRepository
 
 }
