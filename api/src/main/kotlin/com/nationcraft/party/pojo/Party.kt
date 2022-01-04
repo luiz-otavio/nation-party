3ed8f1207fb47c539b3168f5ca458b29f155db41
luiz-otavio <luizfarias2006@hotmail.com>
2022-01-03 23:04:56 -0300

    chore: add search method for member in party

--- api/src/main/kotlin/com/nationcraft/party/pojo/Party.kt
+++ api/src/main/kotlin/com/nationcraft/party/pojo/Party.kt
@@ -1,24 +1,29 @@
 package com.nationcraft.party.pojo
 
 import com.nationcraft.party.pojo.member.PartyMember
+import org.bukkit.entity.Player
 import java.util.*
 import kotlin.collections.ArrayList
 
 data class Party(
     val id: String,
-    val players: ArrayList<PartyMember> = ArrayList()
+    val members: ArrayList<PartyMember> = ArrayList()
 ) {
 
-    fun contains(uniqueId: UUID) = players.any { it.uniqueId == uniqueId }
+    fun contains(uniqueId: UUID) = members.any { it.uniqueId == uniqueId }
 
-    fun getLeader() = players.firstOrNull { it.isLeader }
+    fun getLeader() = members.firstOrNull { it.isLeader }
+
+    fun getMember(player: Player) = getMember(player.uniqueId)
+
+    fun getMember(uniqueId: UUID) = members.firstOrNull { it.uniqueId == uniqueId }
 
     fun addPlayer(player: PartyMember) {
-        players.add(player)
+        members.add(player)
     }
 
     fun removePlayer(player: PartyMember) {
-        players.remove(player)
+        members.remove(player)
     }
 
     override fun equals(other: Any?): Boolean {
