946c281d66ab2be91d8e5f53c1bd64758cefd12f
luiz-otavio <luizfarias2006@hotmail.com>
2022-01-06 17:06:55 -0300

    fix: remove `command-framework` due to compatibility issues

--- api/src/main/kotlin/com/nationcraft/party/PartyPlugin.kt
+++ api/src/main/kotlin/com/nationcraft/party/PartyPlugin.kt
@@ -3,29 +3,20 @@ package com.nationcraft.party
 import com.nationcraft.party.command.PartyCommand
 import com.nationcraft.party.listener.PartyHandler
 import com.nationcraft.party.repository.party.PartyRepository
-import me.saiintbrisson.bukkit.command.BukkitFrame
-import me.saiintbrisson.minecraft.ViewFrame
 import org.bukkit.Bukkit
+import org.bukkit.command.CommandMap
 import org.bukkit.plugin.java.JavaPlugin
 
 class PartyPlugin : JavaPlugin() {
 
-    companion object {
-        fun getInstance() = getPlugin(PartyPlugin::class.java)
-    }
-
-    private lateinit var viewFrame: ViewFrame
-    private lateinit var bukkitFrame: BukkitFrame
-
     override fun onEnable() {
-        viewFrame = ViewFrame(this)
-        bukkitFrame = BukkitFrame(this)
-
-        bukkitFrame.registerCommands(
-            PartyCommand()
-        )
-
         Bukkit.getPluginManager().registerEvents(PartyHandler(), this)
+
+        val commandMap = server.javaClass.getDeclaredField("commandMap").apply { isAccessible = true }
+
+        val map = commandMap.get(server) as CommandMap
+
+        map.register("party", PartyCommand())
     }
 
     fun getRepository() = PartyRepository
